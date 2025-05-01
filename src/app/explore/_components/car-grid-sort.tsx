3432630f349def6031a97e198f466b8ee86bd561
pipe <feliperarnau@gmail.com>
2025-05-01 19:50:42 -0300

    add some icons

--- src/app/explore/_components/car-grid-sort.tsx
+++ src/app/explore/_components/car-grid-sort.tsx
@@ -7,6 +7,7 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Car } from "@/dynamo-db/cars.db";
+import { Car as CarIcon, TrendingUp } from "lucide-react";
 
 type Props = {
   cars: Car[];
@@ -37,7 +38,8 @@ export default function CarSortSelect({ cars, setSortedCars }: Props) {
   return (
     <div className="flex flex-row py-4 gap-2 justify-between items-center">
       <div>
-        <p className="text-sm hidden lg:block pl-[304px]">
+        <p className="text-sm hidden lg:block pl-[310px] lg:translate-y-[16px]">
+          <CarIcon className="text-primary inline mr-2 scale-x-[-100%] translate-y-[-2px] text-lg" />
           Mostrando{" "}
           <span className="font-semibold bg-muted px-2 rounded-sm py-2">
             {cars.length || 0}
@@ -46,8 +48,9 @@ export default function CarSortSelect({ cars, setSortedCars }: Props) {
         </p>
       </div>
       <div>
-        <div className="mb-1 pl-2 text-muted-foreground text-xs">
+        <div className="mb-0 pl-2 text-muted-foreground text-xs">
           Ordenar por
+          <TrendingUp className="text-primary inline ml-1 translate-y-[-1px] w-4" />
         </div>
         <Select value={sortOption} onValueChange={handleSortChange}>
           <SelectTrigger id="sort-select" className="w-[130px] md:w-[200px]">
